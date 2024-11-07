edf9bb27bdfc3c0984731b088ffa25fb00d12483
pratyakshvarshney <pratyakshprakhar@gmail.com>
2024-11-07 19:07:09 +0530

    Create random_file_reading.cpp

---
+++ random_file_reading.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+    fstream file("data.txt", ios::in|ios::out|ios::binary);
+    
+    file.seekp(10);
+    file.write("data",4);
+    
+    file.seekg(10);
+    char buffer[5] = {};
+    file.read(buffer,4);
+    cout<<"data read from offset 10: "<<buffer<<endl;
+    
+    file.close();
+    
+    return 0;
+}
