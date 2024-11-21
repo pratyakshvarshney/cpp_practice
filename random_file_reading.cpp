d55f6628dbbc422d4300fb0791f2906e40995b01
Pratyaksh Varshney <pratyakshprakhar@gmail.com>
2024-11-21 22:48:30 +0530

    Update random_file_reading.cpp

--- random_file_reading.cpp
+++ random_file_reading.cpp
@@ -6,19 +6,17 @@ int main() {
     fstream file("data.txt", ios::in | ios::out | ios::binary);
 
     if (!file) {
-        ofstream("data.txt").close();  
-        file.open("data.txt", ios::in | ios::out | ios::binary);  
+        ofstream("data.txt").close();
+        file.open("data.txt", ios::in | ios::out | ios::binary);
     }
 
-    file.seekp(0); 
-    file.write("1234567890", 10);  
+    file.seekp(10);
+    file.write("data", 4);
 
-    file.seekp(10);  
-    file.write("raja", 4); 
-
-    file.seekg(10);  
-    char buffer[5] = {}; 
+    file.seekg(10);
+    char buffer[5] = {};
     file.read(buffer, 4);
+
     cout << "data read from offset 10: " << buffer << endl;
 
     file.close();
