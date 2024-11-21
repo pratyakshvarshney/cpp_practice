29cedee6a2f6a084662c66a1e6c7d0a866de6254
Pratyaksh Varshney <pratyakshprakhar@gmail.com>
2024-11-21 22:58:41 +0530

    Update random_file_reading.cpp

--- random_file_reading.cpp
+++ random_file_reading.cpp
@@ -23,3 +23,38 @@ int main() {
 
     return 0;
 }
+
+
+// OR
+
+// #include <iostream>
+// #include <fstream>
+// using namespace std;
+
+// void createFileIfNeeded(fstream &file);
+
+// int main() {
+//     fstream file("data.txt", ios::in | ios::out | ios::binary);
+
+//     createFileIfNeeded(file);
+
+//     file.seekp(10);
+//     file.write("data", 4);
+
+//     file.seekg(10);
+//     char buffer[5] = {};
+//     file.read(buffer, 4);
+
+//     cout << "data read from offset 10: " << buffer << endl;
+
+//     file.close();
+
+//     return 0;
+// }
+
+// void createFileIfNeeded(fstream &file) {
+//     if (!file) {
+//         ofstream("data.txt").close();
+//         file.open("data.txt", ios::in | ios::out | ios::binary);
+//     }
+// }
