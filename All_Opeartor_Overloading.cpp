7c4edb6ecc184036ede78b06352aa89549346490
Pratyaksh Varshney <pratyakshprakhar@gmail.com>
2024-11-25 01:08:12 +0530

    Update All_Opeartor_Overloading.cpp

--- All_Opeartor_Overloading.cpp
+++ All_Opeartor_Overloading.cpp
@@ -8,19 +8,21 @@ class complex {
         int real;
         int img;
         
+        complex() {}
+        
         complex (int real, int img) {
             this->real = real;
             this->img = img;
         }
         
         void operator += (complex& c) {
-            this->real += real;
-            this->img += img;
+            this->real += c.real;
+            this->img += c.img;
         } 
         
         void operator -= (complex& c) {
-            this->real -= real;
-            this->img -= img;
+            this->real -= c.real;
+            this->img -= c.img;
         }
          
         bool operator == (complex& c) {
@@ -33,6 +35,14 @@ class complex {
         friend ostream& operator << (ostream& o, complex& c) {
             o << "(" << c.real << ") + i(" << c.img <<")" << endl;
             return o;
+        }
+        
+        friend istream& operator >> (istream& cinn, complex& c) {
+            int i, r;
+            cinn >> r >> i;
+            c.real = r;
+            c.img = i;
+            return cinn;
         }
         
 };
@@ -49,12 +59,22 @@ int main() {
     complex c3(c1);
     c3 += c2;
     complex c4(c1);
-    c4 += c2;
+    c4 -= c2;
     
     cout << "complex c3: ";
     cout << c3;
     cout << "complex c4: ";
     cout << c4;
+    
+    if(c3 == c4)
+        cout << "c3 and c4 are equal" << endl;
+    else
+        cout << "c3 and c4 are not equal" << endl;
+    
+    complex c5;
+    cout << "Enter value for c5: ";
+    cin >> c5;
+    cout << c5;
     
     return 0;
 }
